0806708e7e790d2d4eaf4925f93f7658794ae444
isabel k. lee <hello@isabelklee.co>
2020-01-31 05:08:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,11 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   
-  {railroads: {rent_in_dollars: ""} }
+  {railroads: {rent_in_dollars: {
+    
+       }
+    }
+  }
 end
 
 def monopoly_with_fourth_tier
