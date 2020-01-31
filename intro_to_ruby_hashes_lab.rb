a60f32ba34dc6b56e1f6138bc19d528ab4a2a048
isabel k. lee <hello@isabelklee.co>
2020-01-31 05:17:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ def monopoly_with_third_tier
   
   {railroads:
     {
-      rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, key_two: ""},
+      rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200},
       names: {key_one: "", key_two: "", key_three: "", key_four: ""},
     }
   }
